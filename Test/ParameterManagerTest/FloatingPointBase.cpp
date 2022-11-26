0f1dc6fcb172298cb26d6974315f489fd817abbe
AinoMegumi <aimegu2014@gmail.com>
2022-11-26 13:16:46 +0900

    Opratorの名称を変更

--- Test/ParameterManagerTest/FloatingPointBase.cpp
+++ Test/ParameterManagerTest/FloatingPointBase.cpp
@@ -1,99 +1,99 @@
 ﻿#include <iutest/iutest.hpp>
 #include "FloatingPointBase.hpp"
 template<typename T>
-struct FloatOperator : public ::iutest::Test {};
+struct FloatingPointBaseOperator : public ::iutest::Test {};
 
-IUTEST_TYPED_TEST_CASE(FloatOperator, ::iutest::Types<
+IUTEST_TYPED_TEST_CASE(FloatingPointBaseOperator, ::iutest::Types<
 	float,
 	double,
 	long double
 >);
 
-IUTEST_TYPED_TEST(FloatOperator, equal) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, equal) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 0;
 	const num b = 0;
 	IUTEST_ASSERT(a == b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, ne) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, ne) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 0;
 	const num b = 5;
 	IUTEST_ASSERT(a != b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, large) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, large) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 7;
 	const num b = 0;
 	IUTEST_ASSERT(a > b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, small) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, small) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 125;
 	const num b = 127;
 	IUTEST_ASSERT(a < b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, largeeqA) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, largeeqA) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 17;
 	const num b = 15;
 	IUTEST_ASSERT(a >= b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, largeeqB) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, largeeqB) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 44;
 	const num b = 44;
 	IUTEST_ASSERT(a >= b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, smalleqA) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, smalleqA) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 75;
 	const num b = 98;
 	IUTEST_ASSERT(a <= b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, smalleqB) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, smalleqB) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 55;
 	const num b = 55;
 	IUTEST_ASSERT(a <= b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, plus) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, plus) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 1;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(3, a + b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, minus) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, minus) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(0, a - b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, multiplication) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, multiplication) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(4, a * b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, division) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, division) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(1, a / b);
 }
 
-IUTEST_TYPED_TEST(FloatOperator, mod) {
+IUTEST_TYPED_TEST(FloatingPointBaseOperator, mod) {
 	using num = standard::FloatingPoint<TypeParam>;
 	const num a = 140;
 	const num b = 45;
--- Test/ParameterManagerTest/IntegerBase.cpp
+++ Test/ParameterManagerTest/IntegerBase.cpp
@@ -2,9 +2,9 @@
 #include "IntegerBase.hpp"
 
 template<typename T>
-struct IntegerOperator : public ::iutest::Test{};
+struct IntegerBaseOperator : public ::iutest::Test{};
 
-IUTEST_TYPED_TEST_CASE(IntegerOperator, ::iutest::Types<
+IUTEST_TYPED_TEST_CASE(IntegerBaseOperator, ::iutest::Types<
 	short,
 	int,
 	long,
@@ -15,91 +15,91 @@ IUTEST_TYPED_TEST_CASE(IntegerOperator, ::iutest::Types<
 	unsigned long long
 >);
 
-IUTEST_TYPED_TEST(IntegerOperator, equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, equal) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 0;
 	const num b = 0;
 	IUTEST_ASSERT(a == b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, ne) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, ne) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 0;
 	const num b = 5;
 	IUTEST_ASSERT(a != b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, large) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, large) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 7;
 	const num b = 0;
 	IUTEST_ASSERT(a > b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, small) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, small) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 125;
 	const num b = 127;
 	IUTEST_ASSERT(a < b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, largeeqA) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, largeeqA) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 17;
 	const num b = 15;
 	IUTEST_ASSERT(a >= b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, largeeqB) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, largeeqB) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 44;
 	const num b = 44;
 	IUTEST_ASSERT(a >= b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, smalleqA) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, smalleqA) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 75;
 	const num b = 98;
 	IUTEST_ASSERT(a <= b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, smalleqB) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, smalleqB) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 55;
 	const num b = 55;
 	IUTEST_ASSERT(a <= b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, plus) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, plus) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 1;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(3, a + b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, minus) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, minus) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(0, a - b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, multiplication) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, multiplication) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(4, a * b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, division) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, division) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(1, a / b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, zerodivision) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, zerodivision) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 0;
@@ -112,83 +112,83 @@ IUTEST_TYPED_TEST(IntegerOperator, zerodivision) {
 	}
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, and) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, and) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(2, a & b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, mod) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, mod) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 140;
 	const num b = 45;
 	IUTEST_ASSERT_EQ(5, a % b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, or) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, or) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(2, a | b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, not) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, not) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	IUTEST_ASSERT_EQ(-3, ~a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, xor) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, xor) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(0, a ^ b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, left_shift) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, left_shift) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(8, a << b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, right_shift) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, right_shift) {
 	using num = standard::Integer<TypeParam>;
 	const num a = 2;
 	const num b = 2;
 	IUTEST_ASSERT_EQ(0, a >> b);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, plus_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, plus_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 1;
 	a += 2;
 	IUTEST_ASSERT_EQ(3, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, minus_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, minus_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a -= 2;
 	IUTEST_ASSERT_EQ(0, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, multiplication_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, multiplication_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a *= 2;
 	IUTEST_ASSERT_EQ(4, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, division_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, division_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a /= 2;
 	IUTEST_ASSERT_EQ(1, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, zerodivision_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, zerodivision_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	try {
@@ -200,42 +200,42 @@ IUTEST_TYPED_TEST(IntegerOperator, zerodivision_equal) {
 	}
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, and_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, and_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a &= 2;
 	IUTEST_ASSERT_EQ(2, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, mod_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, mod_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 140;
 	a %= 45;
 	IUTEST_ASSERT_EQ(5, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, or_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, or_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a |= 2;
 	IUTEST_ASSERT_EQ(2, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, xor_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, xor_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a ^= 2;
 	IUTEST_ASSERT_EQ(0, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, left_shift_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, left_shift_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a <<= 2;
 	IUTEST_ASSERT_EQ(8, a);
 }
 
-IUTEST_TYPED_TEST(IntegerOperator, right_shift_equal) {
+IUTEST_TYPED_TEST(IntegerBaseOperator, right_shift_equal) {
 	using num = standard::Integer<TypeParam>;
 	num a = 2;
 	a >>= 2;
