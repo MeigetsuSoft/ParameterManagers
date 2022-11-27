f4c0c01da817efd2f139e5ac58dd8f9e6929d870
AinoMegumi <aimegu2014@gmail.com>
2022-11-27 14:14:45 +0900

    clampの型を制約 ( #3 )
    std::clampはNumberBaseを基礎とした数値管理クラスの比較ができない

--- include/clamp.hpp
+++ include/clamp.hpp
@@ -17,7 +17,8 @@ namespace standard {
 			comp(v, lo) ? lo : comp(hi, v) ? hi : v;
 	}
 #endif
-	template<class T> constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
+	template<class T, std::enable_if_t<std::is_arithmetic<T>::value, std::nullptr_t> = nullptr> 
+	constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
 #if CPP_VER < 201703L
 		return cxx14clamp(v, lo, hi, std::less<>());
 #else
