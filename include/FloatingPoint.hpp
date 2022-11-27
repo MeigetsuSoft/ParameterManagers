926cce8169e5d7569b10982489ae1f328ddea8d2
AinoMegumi <aimegu2014@gmail.com>
2022-11-27 13:13:44 +0900

    db3025eでの修正漏れ分追加

--- include/FloatingPoint.hpp
+++ include/FloatingPoint.hpp
@@ -5,55 +5,106 @@
 
 namespace standard {
 	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline FloatingPoint<T> operator + (const T& n, const FloatingPoint<T>& num) { return FloatingPoint<T>(clamp(n + num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin()); }
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline FloatingPoint<T> operator - (const T& n, const FloatingPoint<T>& num) { return FloatingPoint<T>(clamp(n - num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin()); }
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline FloatingPoint<T> operator * (const T& n, const FloatingPoint<T>& num) { return FloatingPoint<T>(clamp(n * num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin()); }
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline FloatingPoint<T> operator / (const T& n, const FloatingPoint<T>& num) { return FloatingPoint<T>(clamp(n / num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin()); }
-	namespace {
-		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-		constexpr bool eq(const T& n, const FloatingPoint<T>& num) { return std::abs(n - num.Get()) < std::numeric_limits<T>::epsilon(); }
-		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-		constexpr bool ne(const T& n, const FloatingPoint<T>& num) { return !eq(n, num); }
+	inline FloatingPoint<T> operator + (const T& n, const FloatingPoint<T>& num) {
+		return FloatingPoint<T>(clamp(n + num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin());
 	}
 	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline bool operator == (const T& n, const FloatingPoint<T>& num) { return eq(n, num); }
+	inline FloatingPoint<T> operator - (const T& n, const FloatingPoint<T>& num) {
+		return FloatingPoint<T>(clamp(n - num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin());
+	}
 	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline bool operator != (const T& n, const FloatingPoint<T>& num) { return ne(n, num); }
+	inline FloatingPoint<T> operator * (const T& n, const FloatingPoint<T>& num) {
+		return FloatingPoint<T>(clamp(n * num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin());
+	}
 	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline bool operator <  (const T& n, const FloatingPoint<T>& num) { return n < num.Get() && !eq(n, num); }
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline bool operator <= (const T& n, const FloatingPoint<T>& num) { return n <= num.Get(); }
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline bool operator >  (const T& n, const FloatingPoint<T>& num) { return n > num.Get() && !eq(n, num); }
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	inline bool operator >= (const T& n, const FloatingPoint<T>& num) { return n >= num.Get(); }
-
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	constexpr FloatingPoint<T> clamp(const FloatingPoint<T>& v, const FloatingPoint<T>& lo, const FloatingPoint<T>& hi) { return FloatingPoint<T>(clamp<T>(v.Get(), lo.Get(), hi.Get())); }
-
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	constexpr FloatingPoint<T> max(const FloatingPoint<T>& Left, const FloatingPoint<T>& Right) { return FloatingPoint<T>(std::max(Left.Get(), Right.Get())); }
-
-	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-	constexpr FloatingPoint<T> min(const FloatingPoint<T>& Left, const FloatingPoint<T>& Right) { return FloatingPoint<T>(std::min(Left.Get(), Right.Get())); }
-
-	inline FloatingPoint<long double> abs(const FloatingPoint<long double> n) { return FloatingPoint<long double>(std::abs(n.Get())); }
-	inline FloatingPoint<double> abs(const FloatingPoint<double> n) { return FloatingPoint<double>(std::abs(n.Get())); }
-	inline FloatingPoint<float> abs(const FloatingPoint<float> n) { return FloatingPoint<float>(std::abs(n.Get())); }
+	inline FloatingPoint<T> operator / (const T& n, const FloatingPoint<T>& num) {
+		return FloatingPoint<T>(clamp(n / num.Get(), num.GetMin(), num.GetMax()), num.GetMax(), num.GetMin())
+	}
 	namespace {
 		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-		FloatingPoint<T> string_to_float(const std::string s, size_t* Index = 0) { return FloatingPoint<T>(static_cast<T>(std::stold(s, Index))); }
+		constexpr bool eq(const T& n, const FloatingPoint<T>& num) {
+			return std::abs(n - num.Get()) < std::numeric_limits<T>::epsilon();
+		}
 		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-		FloatingPoint<T> wstring_to_float(const std::wstring s, size_t* Index = 0) { return FloatingPoint<T>(static_cast<T>(std::stold(s, Index))); }
+		constexpr bool ne(const T& n, const FloatingPoint<T>& num) {
+			return !eq(n, num);
+		}
 	}
-	inline FloatingPoint<float> stof(const std::string& s, size_t* Index = 0) { return string_to_float<float>(s, Index); }
-	inline FloatingPoint<float> stof(const std::wstring& s, size_t* Index = 0) { return wstring_to_float<float>(s, Index); }
-	inline FloatingPoint<double> stod(const std::string& s, size_t* Index = 0) { return string_to_float<double>(s, Index); }
-	inline FloatingPoint<double> stod(const std::wstring& s, size_t* Index = 0) { return wstring_to_float<double>(s, Index); }
-	inline FloatingPoint<long double> stold(const std::string& s, size_t* Index = 0) { return string_to_float<long double>(s, Index); }
-	inline FloatingPoint<long double> stold(const std::wstring& s, size_t* Index = 0) { return wstring_to_float<long double>(s, Index); }
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	inline bool operator == (const T& n, const FloatingPoint<T>& num) {
+		return eq(n, num);
+	}
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	inline bool operator != (const T& n, const FloatingPoint<T>& num) {
+		return ne(n, num);
+	}
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	inline bool operator <  (const T& n, const FloatingPoint<T>& num) {
+		return n < num.Get() && !eq(n, num);
+	}
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	inline bool operator <= (const T& n, const FloatingPoint<T>& num) {
+		return n <= num.Get();
+	}
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	inline bool operator >  (const T& n, const FloatingPoint<T>& num) {
+		return n > num.Get() && !eq(n, num);
+	}
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	inline bool operator >= (const T& n, const FloatingPoint<T>& num) {
+		return n >= num.Get();
+	}
+
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	constexpr FloatingPoint<T> clamp(const FloatingPoint<T>& v, const FloatingPoint<T>& lo, const FloatingPoint<T>& hi) {
+		return FloatingPoint<T>(clamp<T>(v.Get(), lo.Get(), hi.Get()));
+	}
+
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	constexpr FloatingPoint<T> max(const FloatingPoint<T>& Left, const FloatingPoint<T>& Right) {
+		return FloatingPoint<T>(std::max(Left.Get(), Right.Get()));
+	}
+
+	template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+	constexpr FloatingPoint<T> min(const FloatingPoint<T>& Left, const FloatingPoint<T>& Right) {
+		return FloatingPoint<T>(std::min(Left.Get(), Right.Get()));
+	}
+	inline FloatingPoint<long double> abs(const FloatingPoint<long double> n) {
+		return FloatingPoint<long double>(std::abs(n.Get()));
+	}
+	inline FloatingPoint<double> abs(const FloatingPoint<double> n) {
+		return FloatingPoint<double>(std::abs(n.Get()));
+	}
+	inline FloatingPoint<float> abs(const FloatingPoint<float> n) {
+		return FloatingPoint<float>(std::abs(n.Get()));
+	}
+	namespace {
+		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+		FloatingPoint<T> string_to_float(const std::string s, size_t* Index = 0) {
+			return FloatingPoint<T>(static_cast<T>(std::stold(s, Index)));
+		}
+		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
+		FloatingPoint<T> wstring_to_float(const std::wstring s, size_t* Index = 0) {
+			return FloatingPoint<T>(static_cast<T>(std::stold(s, Index)));
+		}
+	}
+	inline FloatingPoint<float> stof(const std::string& s, size_t* Index = 0) {
+		return string_to_float<float>(s, Index);
+	}
+	inline FloatingPoint<float> stof(const std::wstring& s, size_t* Index = 0) {
+		return wstring_to_float<float>(s, Index);
+	}
+	inline FloatingPoint<double> stod(const std::string& s, size_t* Index = 0) {
+		return string_to_float<double>(s, Index);
+	}
+	inline FloatingPoint<double> stod(const std::wstring& s, size_t* Index = 0) {
+		return wstring_to_float<double>(s, Index);
+	}
+	inline FloatingPoint<long double> stold(const std::string& s, size_t* Index = 0) {
+		return string_to_float<long double>(s, Index);
+	}
+	inline FloatingPoint<long double> stold(const std::wstring& s, size_t* Index = 0) {
+		return wstring_to_float<long double>(s, Index);
+	}
 }
 #endif
--- include/Integer.hpp
+++ include/Integer.hpp
@@ -86,25 +86,57 @@ namespace standard {
 
 	namespace {
 		template<typename T, std::enable_if_t<std::is_signed<T>::value, std::nullptr_t> = nullptr>
-		Integer<T> string_to_signed_integer(const std::string& s, size_t* Index = 0, const int Base = 10) {return Integer<T>(static_cast<T>(std::stoll(s, Index, Base))); }
+		Integer<T> string_to_signed_integer(const std::string& s, size_t* Index = 0, const int Base = 10) {
+			return Integer<T>(static_cast<T>(std::stoll(s, Index, Base)));
+		}
 		template<typename T, std::enable_if_t<std::is_signed<T>::value, std::nullptr_t> = nullptr>
-		Integer<T> wstring_to_signed_integer(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return Integer<T>(static_cast<T>(std::stoll(s, Index, Base))); }
+		Integer<T> wstring_to_signed_integer(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+			return Integer<T>(static_cast<T>(std::stoll(s, Index, Base)));
+		}
 		template<typename T, std::enable_if_t<std::is_unsigned<T>::value, std::nullptr_t> = nullptr>
-		Integer<T> string_to_unsigned_integer(const std::string& s, size_t* Index = 0, const int Base = 10) {return Integer<T>(static_cast<T>(std::stoull(s, Index, Base))); }
+		Integer<T> string_to_unsigned_integer(const std::string& s, size_t* Index = 0, const int Base = 10) {
+			return Integer<T>(static_cast<T>(std::stoull(s, Index, Base)));
+		}
 		template<typename T, std::enable_if_t<std::is_unsigned<T>::value, std::nullptr_t> = nullptr>
-		Integer<T> wstring_to_unsigned_integer(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return Integer<T>(static_cast<T>(std::stoull(s, Index, Base))); }
+		Integer<T> wstring_to_unsigned_integer(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+			return Integer<T>(static_cast<T>(std::stoull(s, Index, Base)));
+		}
 	}
-	inline Integer<int> stoi(const std::string& s, size_t* Index = 0, const int Base = 10) {return string_to_signed_integer<int>(s, Index, Base); }
-	inline Integer<int> stoi(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return wstring_to_signed_integer<int>(s, Index, Base); }
-	inline Integer<long> stol(const std::string& s, size_t* Index = 0, const int Base = 10) {return string_to_signed_integer<long>(s, Index, Base); }
-	inline Integer<long> stol(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return wstring_to_signed_integer<long>(s, Index, Base); }
-	inline Integer<long long> stoll(const std::string& s, size_t* Index = 0, const int Base = 10) {return string_to_signed_integer<long long>(s, Index, Base); }
-	inline Integer<long long> stoll(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return wstring_to_signed_integer<long long>(s, Index, Base); }
-	inline Integer<unsigned int> stoui(const std::string& s, size_t* Index = 0, const int Base = 10) {return string_to_unsigned_integer<unsigned int>(s, Index, Base); }
-	inline Integer<unsigned int> stoui(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return wstring_to_unsigned_integer<unsigned int>(s, Index, Base); }
-	inline Integer<unsigned long> stoul(const std::string& s, size_t* Index = 0, const int Base = 10) {return string_to_unsigned_integer<unsigned long>(s, Index, Base); }
-	inline Integer<unsigned long> stoul(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return wstring_to_unsigned_integer<unsigned long>(s, Index, Base); }
-	inline Integer<unsigned long long> stoull(const std::string& s, size_t* Index = 0, const int Base = 10) {return string_to_unsigned_integer<unsigned long long>(s, Index, Base); }
-	inline Integer<unsigned long long> stoull(const std::wstring& s, size_t* Index = 0, const int Base = 10) {return wstring_to_unsigned_integer<unsigned long long>(s, Index, Base); }
+	inline Integer<int> stoi(const std::string& s, size_t* Index = 0, const int Base = 10) {
+		return string_to_signed_integer<int>(s, Index, Base);
+	}
+	inline Integer<int> stoi(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+		return wstring_to_signed_integer<int>(s, Index, Base);
+	}
+	inline Integer<long> stol(const std::string& s, size_t* Index = 0, const int Base = 10) {
+		return string_to_signed_integer<long>(s, Index, Base);
+	}
+	inline Integer<long> stol(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+		return wstring_to_signed_integer<long>(s, Index, Base);
+	}
+	inline Integer<long long> stoll(const std::string& s, size_t* Index = 0, const int Base = 10) {
+		return string_to_signed_integer<long long>(s, Index, Base);
+	}
+	inline Integer<long long> stoll(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+		return wstring_to_signed_integer<long long>(s, Index, Base);
+	}
+	inline Integer<unsigned int> stoui(const std::string& s, size_t* Index = 0, const int Base = 10) {
+		return string_to_unsigned_integer<unsigned int>(s, Index, Base);
+	}
+	inline Integer<unsigned int> stoui(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+		return wstring_to_unsigned_integer<unsigned int>(s, Index, Base);
+	}
+	inline Integer<unsigned long> stoul(const std::string& s, size_t* Index = 0, const int Base = 10) {
+		return string_to_unsigned_integer<unsigned long>(s, Index, Base);
+	}
+	inline Integer<unsigned long> stoul(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+		return wstring_to_unsigned_integer<unsigned long>(s, Index, Base);
+	}
+	inline Integer<unsigned long long> stoull(const std::string& s, size_t* Index = 0, const int Base = 10) {
+		return string_to_unsigned_integer<unsigned long long>(s, Index, Base);
+	}
+	inline Integer<unsigned long long> stoull(const std::wstring& s, size_t* Index = 0, const int Base = 10) {
+		return wstring_to_unsigned_integer<unsigned long long>(s, Index, Base);
+	}
 }
 #endif
