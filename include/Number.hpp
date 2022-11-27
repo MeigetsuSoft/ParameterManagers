583e1e782d2b261f3dd6b4d925384a64587e0584
AinoMegumi <aimegu2014@gmail.com>
2022-11-27 21:27:12 +0900

    std::fmodの結果をstatic_castでIntegerに変換

--- include/Number.hpp
+++ include/Number.hpp
@@ -24,7 +24,7 @@ namespace standard {
 	template<typename I, typename F, std::enable_if_t<std::is_integral<I>::value&& std::is_floating_point<F>::value, std::nullptr_t> = nullptr>
 	inline Integer<I> operator % (const Integer<I>& i, const FloatingPoint<F>& f) {
 		if (f.Get() < std::numeric_limits<F>::epsilon()) throw std::runtime_error("Divide value is 0");
-		return Integer<I>(std::fmod(static_cast<F>(i.Get()), f.Get()), i.GetMax(), i.GetMin());
+		return Integer<I>(static_cast<I>(std::fmod(static_cast<F>(i.Get()), f.Get())), i.GetMax(), i.GetMin());
 	}
 
 	template<typename I, typename F, std::enable_if_t<std::is_integral<I>::value&& std::is_floating_point<F>::value, std::nullptr_t> = nullptr>
