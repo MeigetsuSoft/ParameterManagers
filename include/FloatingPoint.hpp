9220ef0da84d787c5f4306ad0a0a998a7d0de057
AinoMegumi <aimegu2014@gmail.com>
2022-11-27 13:15:13 +0900

    コピーになっていた部分をconst参照に修正

--- include/FloatingPoint.hpp
+++ include/FloatingPoint.hpp
@@ -69,22 +69,22 @@ namespace standard {
 	constexpr FloatingPoint<T> min(const FloatingPoint<T>& Left, const FloatingPoint<T>& Right) {
 		return FloatingPoint<T>(std::min(Left.Get(), Right.Get()));
 	}
-	inline FloatingPoint<long double> abs(const FloatingPoint<long double> n) {
+	inline FloatingPoint<long double> abs(const FloatingPoint<long double>& n) {
 		return FloatingPoint<long double>(std::abs(n.Get()));
 	}
-	inline FloatingPoint<double> abs(const FloatingPoint<double> n) {
+	inline FloatingPoint<double> abs(const FloatingPoint<double>& n) {
 		return FloatingPoint<double>(std::abs(n.Get()));
 	}
-	inline FloatingPoint<float> abs(const FloatingPoint<float> n) {
+	inline FloatingPoint<float> abs(const FloatingPoint<float>& n) {
 		return FloatingPoint<float>(std::abs(n.Get()));
 	}
 	namespace {
 		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-		FloatingPoint<T> string_to_float(const std::string s, size_t* Index = 0) {
+		FloatingPoint<T> string_to_float(const std::string& s, size_t* Index = 0) {
 			return FloatingPoint<T>(static_cast<T>(std::stold(s, Index)));
 		}
 		template<typename T, std::enable_if_t<std::is_floating_point<T>::value, std::nullptr_t> = nullptr>
-		FloatingPoint<T> wstring_to_float(const std::wstring s, size_t* Index = 0) {
+		FloatingPoint<T> wstring_to_float(const std::wstring& s, size_t* Index = 0) {
 			return FloatingPoint<T>(static_cast<T>(std::stold(s, Index)));
 		}
 	}
